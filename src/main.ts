d5e4cc7aae2f5f562f0fe501471e1cefc0db3216
4EEZE <de3terrp5@gmail.com>
2025-03-23 01:16:10 +0300

    all sections of the main page are completed

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,6 @@ import { mount } from 'svelte'
 import './app.css'
 import App from './App.svelte'
 import WeHaveSlider from './components/we_have_slider.svelte'
-import BannerSlider from './components/banner_slider.svelte'
 
 const app = mount(App, {
   target: document.getElementById('app')!,
@@ -10,10 +9,6 @@ const app = mount(App, {
 
 const we_have_slider = mount(WeHaveSlider, {
   target: document.getElementById('we_have_slider')!,
-})
-
-const banner_slider = mount(BannerSlider, {
-  target: document.getElementById('banner')!,
 })
 
 export default app
